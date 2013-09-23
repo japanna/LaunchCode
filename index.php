720d8683e5f1e03cab68e5c59bc29ae90605b562
Anna McKelvey <anna.e.mckelvey@gmail.com>
2013-09-23 15:40:40 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+
+<html> 
+   <li><a href=""  target="_blank"> </a></li>
+  <head>
+    <title>LaCo | Resources</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="fonts/MyFontsWebfontsKit.css">
+    <link rel="stylesheet" type="text/css" href="fonts/MyFontsWebfontsKit3.css">
+  </head>
+  <body>
+    <div id="resource_list">
+    <h1>Git</h1>
+    <ul>
+      <li><a href="http://training.github.com/resources/videos/" target="_blank">github training videos</a></li>
+      <li><a href="http://try.github.io/levels/1/challenges/1"  target="_blank">15-minute interactive intro to Git</a></li>
+    </ul>
+    <h1>Intro to computer science</h1>
+    <ul>
+      <li><a href="http://cs50.tv/2012/fall/" target="_blank">Intro to CS, Harvard</a></li>
+      <li><a href="http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-00-introduction-to-computer-science-and-programming-fall-2008/index.htm" target="_blank">Intro to CS from MIT</a></li>
+      <li><a href="https://www.edx.org/course/uc-berkeley/cs-169-1x/software-service/993" target="_blank">Software as a Service, agile/pair programming, Berkeley</a></li>
+      <li><a href="http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/index.htm" target="_blank">Principles of Computation, MIT</a></li>
+    </ul>
+    <h1>Object Oriented Programming</h1>
+    <ul>
+      <li><a href="https://www.udacity.com/course/cs046" target="_blank">Intro to object oriented programming, Udacity</a></li>
+      <li><a href="http://www.greenteapress.com/thinkapjava/" target="_blank">e-book: Intro to Java </a></li>
+    </ul>
+    <h1>Ruby/Rails</h1>
+    <ul>
+      <li><a href="http://tryruby.org/levels/1/challenges/0" target="_blank">15-min interactive intro to Ruby</a></li>
+      <li><a href="http://www.codeschool.com/courses/rails-for-zombies-redux" target="_blank">Quick intro to Rails</a></li>
+      <li><a href="http://www.codeschool.com/code_tv/heroku" target="_blank">Deploy Rails projects on Heroku </a></li>
+    </ul>
+    <h1>Python</h1>
+    <ul>
+      <li><a href="http://learnpythonthehardway.org/book">E-book: Learn Python the Hard Way</a></li>
+    </ul>
+    <h1>Mobile apps</h1>
+    <ul>
+      <li><a href="http://cs76.tv/2011/spring/" target="_blank">Building mobile apps for iOS and Android, Harvard</a></li>
+      <li><a href="https://developers.google.com/university/courses/">Google Code University</a></li>
+    </ul>
+    <h1>Test your skills / practice tests</h1>
+    <ul>
+      <li><a href="http://codility.com/train/" target="_blank">Codility training site</a></li>
+      <li><a href="https://www.hackerrank.com" target="_blank">HackerRank - contests and challenges</a></li>
+    </ul>
+
+    <h1>Debugging/software testing</h1>
+    <ul>
+      <li><a href="https://www.udacity.com/course/cs259" target="_blank">Software debugging, Udacity</a></li>
+      <li><a href="https://www.udacity.com/course/cs258" target="_blank">Software testing, Udacity</a></li>
+    </ul>
+    <h1>General resources</h1>
+    <ul>
+      <li><a href="http://www.codecademy.com/" target="_blank">Code Academy</a></li>
+      <li><a href="https://www.edx.org/">EdX</a></li>
+      <li><a href="https://www.coursera.org/">Coursera</a></li>
+      <li><a href="https://p2pu.org/en/schools/school-of-webcraft/">Mozilla School of Webcraft</a></li>
+      <li><a href="http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/">MIT OpenCourseWare</a></li>
+      <li><a href="http://see.stanford.edu/default.aspx">Stanford Engineering Everywhere</a></li>
+      <li><a href="https://developers.google.com/university/courses/">Google Code University</a></li>
+      <li><a href="http://www.udacity.com/" target="_blank">Udacity</a></li>
+    </ul>
+    <h1>Pay to learn</h1>
+    <ul>
+      <li><a href="http://www.codeschool.com/">Code School</a></li>
+      <li><a href="http://teamtreehouse.com/">Treehouse</a></li>
+    </ul>
+	</div>
+</body>
+</html>	
